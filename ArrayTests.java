d94d240f30825a1b8e6444477f7a3134e79a3b21
Sierra <smyers24816@gmail.com>
2023-04-19 15:43:24 -0700

    fixed ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,15 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 0,1,2,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3,2,1,0 }, input1);
+	}
+  @Test
+  public void testReversed2() {
+    int[] input1 = {0,1,2,3 };
+    assertArrayEquals(new int[]{ 3,2,1,0}, ArrayExamples.reversed(input1));
+  }
 }
